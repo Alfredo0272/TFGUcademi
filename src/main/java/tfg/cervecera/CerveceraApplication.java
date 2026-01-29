335e5897f9c5194e599b8b00038017aab79d9a39
Alfredo <alfredo.realizador@gmail.com>
2026-01-29 11:51:39 +0100

    Initial Spring Boot setup

---
+++ src/main/java/tfg/cervecera/CerveceraApplication.java
@@ -0,0 +1,13 @@
+package tfg.cervecera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerveceraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerveceraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfg/cervecera/CerveceraApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.cervecera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerveceraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
